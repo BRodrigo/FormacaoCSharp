5f94d2620b6a07245fd9eb96711714aa42b4c6a5
brcaetanoferreira <brcaetanoferreira@LPT-LE1059>
2020-09-01 14:43:08 +0100

    Add new files, started a new module the C#

---
+++ NewByteBank/NewByteBank/Funcionarios/Diretor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewByteBank.Funcionarios
+{
+    public class Diretor : Funcionario
+    {
+     
+        public override double GetBonificacao()
+        {
+        
+            return Salario + base.GetBonificacao();
+        }
+    }
+}
